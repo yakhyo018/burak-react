e8171953f74184c45afacdba6c2dbd6bd37735e9
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-07-20 16:40:47 +0700

    feat: develop redux slicer and selector

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,13 +1,37 @@
-import React from "react";
-import Statistics from "./Statistics";
-import PopularDishes from "./PopularDishes";
-import NewDishes from "./NewDishes";
-import Advertisement from "./Advertisement";
+import React, { useEffect } from "react";
 import ActiveUsers from "./ActiveUsers";
+import Advertisement from "./Advertisement";
 import Events from "./Events";
+import NewDishes from "./NewDishes";
+import PopularDishes from "./PopularDishes";
+import Statistics from "./Statistics";
 import "../../../css/home.css";
 
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+
+/** REDUX SLICE & SELECTOR **/
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({ popularDishes }),
+);
+
 export default function HomePage() {
+  // selector: DAta => Store
+  const { setPopularDishes } = actionDispatch(useDispatch());
+  const { popularDishes } = useSelector(popularDishesRetriever);
+
+  useEffect(() => {
+    // Backernd serverdan Data qabul qilamiz
+    // Backenddan qabul qilgan malumotni slice Storega joylaymiz
+  }, []);
   return (
     <div className={"homepage"}>
       <Statistics />
@@ -19,3 +43,25 @@ export default function HomePage() {
     </div>
   );
 }
+
+// import React from "react";
+// import Statistics from "./Statistics";
+// import PopularDishes from "./PopularDishes";
+// import NewDishes from "./NewDishes";
+// import Advertisement from "./Advertisement";
+// import ActiveUsers from "./ActiveUsers";
+// import Events from "./Events";
+// import "../../../css/home.css";
+
+// export default function HomePage() {
+//   return (
+//     <div className={"homepage"}>
+//       <Statistics />
+//       <PopularDishes />
+//       <NewDishes />
+//       <Advertisement />
+//       <ActiveUsers />
+//       <Events />
+//     </div>
+//   );
+// }
