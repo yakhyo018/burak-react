749cd4b185cc90203c89eff6bbe6e051a336cddd
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-07-24 11:14:24 +0700

    fix: reducer fixed

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,12 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import reduxLogger from "redux-logger";
+import HomePageReducer from "./screens/homePage/slice";
 
 export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     // @ts-ignore
     getDefaultMiddleware().concat(reduxLogger),
-  reducer: {},
+  reducer: { homePage: HomePageReducer },
 });
 
 export type AppDispatch = typeof store.dispatch;
